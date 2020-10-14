6d6abb041e5070b5edfcca474d84b705cf4ebb08
muskaanshraogi <muskaanshraogi123@gmail.com>
2020-10-14 20:11:44 +0530

    Profile page added

--- django_learning_blog/urls.py
+++ django_learning_blog/urls.py
@@ -6,6 +6,7 @@ from user import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='user-profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('blog.urls')),
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -16,3 +17,6 @@ def register(request):
     
     return render(request, 'user/register.html', { 'form' : form })
 
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
